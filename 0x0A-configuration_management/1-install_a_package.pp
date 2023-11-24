bc704138fe34c94d810c3c1eb18ab2726113187a
Yoseph Tamirat <yosephtamir@gmail.com>
2023-11-25 00:29:57 +0300

    fd

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,8 +4,10 @@ package { 'python3-pip':
   ensure => 'installed',
 }
 
+# excutes the installation
+
 exec { 'flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
+  command => '/usr/bin/pip3 install flask==2.1.0',
   path    => ['/usr/bin'],
   require => Package['python3-pip'],
 }
