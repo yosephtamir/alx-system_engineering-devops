002c80fad0b3918cc0231b0c9ca4bfcc93602693
Yoseph Tamirat <yosephtamir@gmail.com>
2023-11-26 21:09:14 +0300

    s'

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+#ssh password req
+include 'stdlib'
+
+file_line { 'password':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => '^PasswordAuthentication'
+}
+
+file_line { 'identity':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => '^IdentityFile'
+}
