0615151ef17fdd7a4e65e43ebb7287e7229debb5
Yoseph Tamirat <yosephtamir@gmail.com>
2024-01-02 17:25:58 +0300

    all user

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This is used to export a json file of a user"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """the script runs if not imported"""
+    url = 'https://jsonplaceholder.typicode.com/'
+    tasks = []
+    dictfile = {}
+    allDict = {}
+    urlLength = 'https://jsonplaceholder.typicode.com/users'
+    reqLength = requests.get(urlLength).json()
+
+    for count in range(len(reqLength)):
+        req = requests.get(url + "users/" + str(count + 1)).json()
+        todo = requests.get(url + "todos",
+                            params={"userId": str(count + 1)}).json()
+        tasks = []
+        for i in todo:
+            newDict = {"task": "", "completed": "", "username": ""}
+
+            newDict["task"] = i["title"]
+            newDict["completed"] = i["completed"]
+            newDict["username"] = req["username"]
+            # Used to add each dictionaries to list
+            tasks.append(newDict)
+        dictfile["{}".format(count + 1)] = tasks
+
+    with open("todo_all_employees.json", "w", encoding="utf-8") as jfile:
+        json.dump(dictfile, jfile)
