a8586fbfea0e762d80d40a36f75b9d23c5cbd864
Yoseph Tamirat <yosephtamir@gmail.com>
2024-01-16 23:07:31 +0300

    hor sub

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Used to return the number of subscribers available"""
+
+
+def number_of_subscribers(subreddit):
+    """ returns the length of subredits subscribers"""
+    from requests import get
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    headers = {'user-agent': 'my-app/0.0.1'}
+
+    req = get(url, headers=headers, allow_redirects=False)
+
+    if req.status_code != 200:
+        return 0
+
+    try:
+        jsonf = req.json()
+    except ValueError:
+        return 0
+
+    data = jsonf.get("data")
+
+    if data:
+        sub_count = data["subscribers"]
+        if sub_count:
+            return sub_count
+
+    return 0
