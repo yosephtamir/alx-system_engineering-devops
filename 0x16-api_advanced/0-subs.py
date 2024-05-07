2f7b28f6a1ca7a736c6c538d8cb57d9201ecf5fc
se <Yoseph Tamirat>
2024-05-07 22:49:46 +0300

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,27 +4,13 @@ from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """ returns the length of subredits subscribers"""
-
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
-    headers = {'user-agent': 'my-app/0.0.1'}
-
-    req = get(url, headers=headers, allow_redirects=False)
-
-    if req.status_code != 200:
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-    try:
-        jsonf = req.json()
-    except ValueError:
-        return 0
-
-    data = jsonf.get("data")
-
-    if data:
-        sub_count = data["subscribers"]
-        if sub_count:
-            return sub_count
-
-    return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
