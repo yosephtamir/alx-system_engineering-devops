c79252864cc88d6ed34059adfe0e0c25ca891bb0
se <Yoseph Tamirat>
2024-05-07 22:35:09 +0300

    recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,7 +3,7 @@
 from requests import get
 
 
-def recurse(subreddit, hot_list=[], after=""):
+def recurse(subreddit, hot_list=[], after="", count=0):
     """A recursive function that queries the Reddit API
     and returns a list containing the titles of all hot
     articles for a given subreddit. If no results are found for
