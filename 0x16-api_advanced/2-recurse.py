22b51005196db9c0a22d52f87b19987780586bfc
se <Yoseph Tamirat>
2024-05-07 22:27:44 +0300

    recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,36 +8,26 @@ def recurse(subreddit, hot_list=[], after=""):
     and returns a list containing the titles of all hot
     articles for a given subreddit. If no results are found for
     the given subreddit, the function should return None."""
-
-    if after is None:
-        return hot_list
-
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-
- 
-
-    req = get(url, headers={'user-agent': 'my-app/0.0.1'},
-            params={'limit': 100, 'after': after}, allow_redirects=False)
-
-    if req.status_code != 200:
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         return None
 
-    try:
-        jsonf = req.json()
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
 
-    except ValueError:
-        return None
-
-    try:
-
-        data = jsonf.get("data")
-        after = data.get("after")
-        children = data.get("children")
-        for child in children:
-            post = child.get("data")
-            hot_list.append(post.get("title"))
-
-    except:
-        return None
-
-    return recurse(subreddit, hot_list, after)
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
