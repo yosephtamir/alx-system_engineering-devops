3cee9df8abead1ffce64f848bf9d8f067351f725
se <Yoseph Tamirat>
2024-05-08 13:22:43 +0300

    webs Readme

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,9 +0,0 @@
-# Debugging the error
-exec { 'fix-wordpress':
-  environment => ['DIR=/var/www/html/wp-settings.php',
-                  'OLD=phpp',
-                  'NEW=php'],
-  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR',
-  path        => ['/usr/bin', '/bin'],
-  returns     => [0, 1]
-}
